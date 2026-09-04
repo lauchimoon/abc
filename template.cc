6f88ced48afda4e6a33fde3120fa683f92f49df9
lauchimoon <laurentino.luna06@gmail.com>
2026-09-04 11:29:45 -0300

    Add template

---
+++ template.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define rdv(a,n) for (auto &x : a) cin >> x;
+#define mp make_pair
+#define pb push_back
+using ii = pair<int,int>;
+using ll = long long;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
