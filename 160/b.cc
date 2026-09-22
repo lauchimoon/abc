db06c0ef5a0e7ca864a26df37671385ba82469f8
lauchimoon <laurentino.luna06@gmail.com>
2026-09-22 16:03:46 -0300

    Add 160

---
+++ 160/b.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define rdv(a,n) for (auto &x : a) cin >> x;
+#define mp make_pair
+#define pb push_back
+using ii = pair<int,int>;
+using ll = long long;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    ll ans = 0;
+    if (x/500) {
+        ans += 1000*(x/500);
+        x -= 500*(x/500);
+    }
+    if (x/5) {
+        ans += 5*(x/5);
+        x -= 5*(x/5);
+    }
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
+
