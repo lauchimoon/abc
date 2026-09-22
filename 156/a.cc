c5b26165ddb0a508513f701e7a7286b7d5adc738
lauchimoon <laurentino.luna06@gmail.com>
2026-09-21 22:34:01 -0300

    Add 156

---
+++ 156/a.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define rdv(a,n) for (auto &x : a) cin >> x;
+#define mp make_pair
+#define pb push_back
+using ii = pair<int,int>;
+using ll = long long;
+
+void solve()
+{
+    int n,r;
+    cin >> n >> r;
+    cout << ((n >= 10)? r : r+100*(10-n)) << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
+
---
+++ 156/b.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define rdv(a,n) for (auto &x : a) cin >> x;
+#define mp make_pair
+#define pb push_back
+using ii = pair<int,int>;
+using ll = long long;
+
+void solve()
+{
+    int n,k;
+    cin >> n >> k;
+    int ans = 0;
+    while (n!=0) {
+        ++ans;
+        n/=k;
+    }
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
+
